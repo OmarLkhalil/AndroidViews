8764775bd16b1c950f9c2edb7c3e67718a3bf265
omarLkhalil <42020332@hti.edu.eg>
2022-09-19 00:51:49 +0200

    Finished

---
+++ app/src/main/java/com/example/routscreenz/FacebookLogin.kt
@@ -0,0 +1,11 @@
+package com.example.routscreenz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacebookLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_login)
+    }
+}
---
+++ app/src/main/java/com/example/routscreenz/FacebookPost.kt
@@ -0,0 +1,18 @@
+package com.example.routscreenz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.routscreenz.adapters.PostAdapterr
+import com.example.routscreenz.models.PostModel
+
+class FacebookPost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_post)
+    }
+
+
+
+}
+
